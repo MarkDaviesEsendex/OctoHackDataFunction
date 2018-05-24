efe8e4a971eb711e872d79f355fa9d3af8eded44
Riordan Panayides <riordan.panayides@esendex.com>
2018-05-24 11:38:09 +0100

    Return some dummy data

--- UsageDataProcessor/UsageDataProcessor/Function1.cs
+++ UsageDataProcessor/UsageDataProcessor/Function1.cs
@@ -14,17 +14,13 @@ namespace UsageDataProcessor
         [FunctionName("Function1")]
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
+            var myObj = new { spentYesterday = 1.234, avgLastMonth = 3.456 };
+            var jsonToReturn = JsonConvert.SerializeObject(myObj);
 
-            string name = req.Query["name"];
-
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+            };
         }
     }
 }
